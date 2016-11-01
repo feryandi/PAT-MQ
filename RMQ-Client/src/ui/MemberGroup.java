b754d24a4723fac2e537afca2669ee5f0381c728
Feryandi Nurdiantoro <feryandi.n@gmail.com>
2016-11-01 13:32:23 +0700

    Initial Member Group Func

--- RMQ-Client/src/ui/MemberGroup.java
+++ RMQ-Client/src/ui/MemberGroup.java
@@ -5,16 +5,26 @@
  */
 package ui;
 
+import core.Client;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 /**
  *
  * @author feryandi
  */
 public class MemberGroup extends javax.swing.JFrame {
 
+    Client c;
+    
     /**
      * Creates new form MemberGroup
      */
     public MemberGroup() {
+        c = Client.getInstance();
         initComponents();
     }
 
@@ -29,8 +39,8 @@ public class MemberGroup extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         list_member = new javax.swing.JList<>();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        btn_remove = new javax.swing.JButton();
+        btn_new = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -41,9 +51,14 @@ public class MemberGroup extends javax.swing.JFrame {
         });
         jScrollPane1.setViewportView(list_member);
 
-        jButton1.setText("REMOVE");
+        btn_remove.setText("REMOVE");
+        btn_remove.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_removeActionPerformed(evt);
+            }
+        });
 
-        jButton2.setText("ADD NEW");
+        btn_new.setText("ADD NEW");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -54,9 +69,9 @@ public class MemberGroup extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jScrollPane1)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btn_new, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, Short.MAX_VALUE)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(btn_remove, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -66,17 +81,70 @@ public class MemberGroup extends javax.swing.JFrame {
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 249, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
+                    .addComponent(btn_remove)
+                    .addComponent(btn_new))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btn_removeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_removeActionPerformed
+        try {
+            JSONObject o = new JSONObject();
+            o.put("method", "del_member");
+            
+            JSONObject p = new JSONObject();
+            p.put("userid", list_member.getSelectedValue());
+            p.put("group", 0); // TO-DO
+            
+            o.put("params", p);   
+            
+            String response = c.call(o.toJSONString());          
+            
+            /* Check Response */
+            JSONParser parser = new JSONParser();        
+            JSONObject r = (JSONObject) parser.parse(response);
+            String status = (String) r.get("status");
+            if (status.equals("success")) {
+                
+            }
+            
+        } catch (Exception ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_btn_removeActionPerformed
+
+    public void PopulateMembers() throws Exception {
+        String[] new_data = {""};
+        
+        JSONObject o = new JSONObject();
+        o.put("method", "get_member");
+
+        JSONObject p = new JSONObject();
+        p.put("groupid", c.userid); // TO-DO!!
+        o.put("params", p);            
+
+        String response = c.call(o.toJSONString());
+        System.out.println("Get Member reponse: " + response);
+
+        /* Check Response */
+        JSONParser parser = new JSONParser();
+        JSONObject r = (JSONObject) parser.parse(response);
+        String status = (String) r.get("status");
+        if (status.equals("success")) {
+            JSONArray json_array = (JSONArray) r.get("data");
+            new_data = new String[json_array.size()];
+            for (int i=0; i<json_array.size(); i++){
+                new_data[i] = json_array.get(i).toString();
+            }
+        }
+        list_member.setListData(new_data);
+    }
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton btn_new;
+    private javax.swing.JButton btn_remove;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JList<String> list_member;
     // End of variables declaration//GEN-END:variables
